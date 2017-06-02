d1e9bca5e137a543558c25392ba76efa165e8a8a
Panji Yudasetya Wiwaha <panji@sense-os.nl>
2017-06-02 11:16:25 +0700

    put documentation on connection observer

--- RNFitAndAwareness/android/input-kit/src/main/java/nl/sense_os/input_kit/ConnectionObserver.java
+++ RNFitAndAwareness/android/input-kit/src/main/java/nl/sense_os/input_kit/ConnectionObserver.java
@@ -15,15 +15,32 @@ import nl.sense_os.input_kit.listeners.InputKitConnectionListener;
 public class ConnectionObserver {
     private final Map<String, InputKitConnectionListener> mObserver = new HashMap<>();
 
+    /**
+     * To avoid {@link java.util.ConcurrentModificationException} instead of removing the elements,
+     * just set observer value to null.
+     *
+     * @param eventName Observer event name
+     */
     private void removeObserver(@Nullable String eventName) {
         mObserver.put(eventName, null);
     }
 
+    /**
+     * Add Input Kit connection observer.
+     * @param eventName Observer event name
+     * @param listener  Input Kit connection listener
+     */
     public void addObserver(@NonNull String eventName,
                             @NonNull InputKitConnectionListener listener) {
         mObserver.put(eventName, listener);
     }
 
+    /**
+     * Notifying all of connection observers and remove it immediately from the tree elements to avoid
+     * Illegal callback invocation from React Native module error.
+     * @param isAccessible  True if an input kit is accessible, False otherwise
+     * @param message       Additional connection messages
+     */
     public void notifyAll(boolean isAccessible, @NonNull String message) {
         for (Map.Entry<String, InputKitConnectionListener> consumer : mObserver.entrySet()) {
             InputKitConnectionListener listener = consumer.getValue();
